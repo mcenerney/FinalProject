fb54e990d716e77d31d608ad32aecae74313fb03
cayorne <39387330+cayorne@users.noreply.github.com>
2024-12-02 11:09:32 -0500

    first

---
+++ src/assets/data.js
@@ -0,0 +1,42 @@
+export const data = [
+    {
+        question: "What is 2+2?",
+        option1: "4",
+        option2: "6",
+        option3: "8",
+        option4: "5",
+        ans: 1
+    },
+    {
+        question: "Who was the first president of the USA?",
+        option1: "Thomas Jefferson",
+        option2: "Abraham Lincoln",
+        option3: "George Washington",
+        option4: "Ben Franklin",
+        ans: 3
+    },
+    {
+        question: "What state is Kansas City located in?",
+        option1: "Kansas",
+        option2: "Missouri",
+        option3: "Arkansas",
+        option4: "Minnesota",
+        ans: 2
+    },
+    {
+        question: "What is 4 x 2?",
+        option1: "8",
+        option2: "12",
+        option3: "6",
+        option4: "4",
+        ans: 1
+    },
+    {
+        question: "What is the capitol of Connecticut?",
+        option1: "Waterbury",
+        option2: "Hartford",
+        option3: "New Haven",
+        option4: "Bridgeport",
+        ans: 2
+    }
+]
